9239d3ac4ddae75ca8bc3c25ea6d25d234650ad2
Bundit Nuntates <silkyland@gmail.com>
2018-06-29 00:43:41 +0700

    add local state

--- src/components/FormSubmit.js
+++ src/components/FormSubmit.js
@@ -3,17 +3,33 @@ import React, { Component } from "react";
 class FormSumit extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      message: ""
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSumitForm = this.handleSumitForm.bind(this);
   }
+  handleInputChange(e) {
+    this.setState({ message: e.target.value });
+  }
+
+  handleSumitForm(e) {
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div style={{ margin: 2 }}>
-        <from action="" style={{ display: "flex" }}>
-          <input type="text" style={{ flex: 9, height: 30 }} />
-          <button style={{ height: 35, flex: 1 }}>
+        <form onSubmit={this.handleSumitForm} style={{ display: "flex" }}>
+          <input
+            type="text"
+            onChange={this.handleInputChange}
+            style={{ flex: 9, height: 30 }}
+          />
+          <button type="submit" style={{ height: 35, flex: 1 }}>
             <strong>Add to List</strong>
           </button>
-        </from>
+        </form>
       </div>
     );
   }
--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -3,86 +3,49 @@ import React, { Component } from "react";
 class ListItems extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      todos: [
+        { id: 1, name: "Shopping", complete: false },
+        { id: 2, name: "Swimming", complete: false },
+        { id: 3, name: "Watch Movie", complete: false },
+        { id: 4, name: "Dinner", complete: false },
+        { id: 5, name: "DOTA", complete: false }
+      ]
+    };
   }
+
+  handleCheckboxCheck(index, complete) {
+    let oldState = this.state.todos;
+    oldState[index].complete = complete;
+    this.setState({ todos: oldState });
+  }
+
   render() {
     return (
       <React.Fragment>
-        <div
-          style={{
-            backgroundColor: "#fefefe",
-            borderColor: "#ccc",
-            borderWidth: 1,
-            borderStyle: "solid",
-            borderRadius: 5,
-            margin: 2,
-            padding: 5,
-            paddingTop: 10,
-            paddingBottom: 19
-          }}
-        >
-          <input type="checkbox" checked /> <s> Shopping</s>
-        </div>
-        <div
-          style={{
-            backgroundColor: "#fefefe",
-            borderColor: "#ccc",
-            borderWidth: 1,
-            borderStyle: "solid",
-            borderRadius: 5,
-            margin: 2,
-            padding: 5,
-            paddingTop: 10,
-            paddingBottom: 19
-          }}
-        >
-          <input type="checkbox" /> Swimming
-        </div>
-        <div
-          style={{
-            backgroundColor: "#fefefe",
-            borderColor: "#ccc",
-            borderWidth: 1,
-            borderStyle: "solid",
-            borderRadius: 5,
-            margin: 2,
-            padding: 5,
-            paddingTop: 10,
-            paddingBottom: 19
-          }}
-        >
-          <input type="checkbox" /> Watch Movie
-        </div>
-        <div
-          style={{
-            backgroundColor: "#fefefe",
-            borderColor: "#ccc",
-            borderWidth: 1,
-            borderStyle: "solid",
-            borderRadius: 5,
-            margin: 2,
-            padding: 5,
-            paddingTop: 10,
-            paddingBottom: 19
-          }}
-        >
-          <input type="checkbox" /> Dinner
-        </div>
-        <div
-          style={{
-            backgroundColor: "#fefefe",
-            borderColor: "#ccc",
-            borderWidth: 1,
-            borderStyle: "solid",
-            borderRadius: 5,
-            margin: 2,
-            padding: 5,
-            paddingTop: 10,
-            paddingBottom: 19
-          }}
-        >
-          <input type="checkbox" /> DOTA
-        </div>
+        {this.state.todos.map((todo, index) => (
+          <div
+            style={{
+              backgroundColor: "#fefefe",
+              borderColor: "#ccc",
+              borderWidth: 1,
+              borderStyle: "solid",
+              borderRadius: 5,
+              margin: 2,
+              padding: 5,
+              paddingTop: 10,
+              paddingBottom: 19
+            }}
+            key={todo.id}
+          >
+            <input
+              type="checkbox"
+              checked={todo.complete}
+              onChange={() => this.handleCheckboxCheck(index, !todo.complete)}
+            />{" "}
+            {!todo.complete ? todo.name : <s> {todo.name}</s>}
+          </div>
+        ))}
       </React.Fragment>
     );
   }
